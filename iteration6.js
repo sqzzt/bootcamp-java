472174f6419b6fa33c9815d3383a12d66a2d4a23
sqzzt <midominguezro@hotmail.com>
2023-10-22 15:57:27 +0200

    last iteration 6

---
+++ iteration6.js
@@ -0,0 +1,21 @@
+
+for (i = 0; i < 10 ; i++){
+    console.log(i)
+}
+
+for (i = 0 ; i < 10 ; i++) {
+    if ( i % 2 == 0){
+        console.log(i)
+    }
+}
+
+for (i = 0 ; i <= 10 ; i++){
+    if (i < 9 ) {
+        console.log('Contando ovejas')
+    } else if (i == 10) {
+        console.log('Dormido!')
+    };
+
+    
+
+    };
